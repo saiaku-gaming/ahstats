30ce46a15aefa25b04b36ed1a01dafcd35f31cac
Mio Engel <mioengel@gmail.com>
2023-12-23 12:39:19 +0100

    Adds indexes to the table

---
+++ sqlscripts/Script0004 - Creates indexes.sql
@@ -0,0 +1,3 @@
+﻿CREATE INDEX idx_auction_entry_auction_id ON auction_entry(auction_id);
+CREATE INDEX idx_auction_entry_time_left ON auction_entry(time_left);
+CREATE INDEX idx_auction_entry_id ON auction_entry(id);
